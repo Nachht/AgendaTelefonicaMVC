d68030e130b62b90848233990371aa9bb247baf3
Lina María Gallardo Corrales <linamgallardoc3@gmail.com>
2026-07-21 16:38:25 -0500

    iIntegrar funcionalidades con la vista

--- src/main/java/com/hackathon/agenda/Main.java
+++ src/main/java/com/hackathon/agenda/Main.java
@@ -1,12 +1,15 @@
 package com.hackathon.agenda;
 
+import com.hackathon.agenda.controlador.AgendaControlador;
+import com.hackathon.agenda.modelo.Agenda;
 import com.hackathon.agenda.vista.VentanaPrincipal;
 
 public class Main {
 
     public static void main(String[] args) {
-
+        Agenda agenda = new Agenda();
         VentanaPrincipal ventana = new VentanaPrincipal();
+        AgendaControlador agendaControlador = new AgendaControlador(agenda, ventana);
         ventana.setVisible(true);
 
     }
