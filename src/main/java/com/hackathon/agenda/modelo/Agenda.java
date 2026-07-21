6677fd7eff8676e5469a5cd6fecaa3597c24712a
Fabian <fabian.rodriguezc@cun.edu.co>
2026-07-21 15:24:20 -0500

    Implementación inicial de Agenda

--- src/main/java/com/hackathon/agenda/modelo/Agenda.java
+++ src/main/java/com/hackathon/agenda/modelo/Agenda.java
@@ -1,4 +1,63 @@
 package com.hackathon.agenda.modelo;
 
+import java.util.ArrayList;
+
 public class Agenda {
+
+
+    private ArrayList<Contacto> contactos;
+    private int tamanoMaximo;
+
+
+    public Agenda() {
+        tamanoMaximo = 10;
+        contactos = new ArrayList<>();
+    }
+
+
+    public Agenda(int tamanoMaximo) {
+        this.tamanoMaximo = tamanoMaximo;
+        contactos = new ArrayList<>();
+    }
+
+
+    public boolean agendaLlena() {
+
+    }
+
+
+    public int espaciosLibres() {
+
+    }
+
+
+    public boolean existeContacto(Contacto contacto) {
+
+    }
+
+
+    public void añadirContacto(Contacto contacto) {
+
+    }
+
+
+    public void listarContactos() {
+
+    }
+
+
+    public Contacto buscarContacto(String nombre, String apellido) {
+
+    }
+
+
+    public void eliminarContacto(Contacto contacto) {
+
+    }
+
+
+    public void modificarTelefono(String nombre, String apellido, String nuevoTelefono) {
+
+    }
+
 }
